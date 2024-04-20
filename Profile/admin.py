a52d2af1e4903bb9ea5a0e0db27cd847702a61da
Muhammad Abbas <kabbas942@gmail.com>
2024-04-20 22:35:50 +0500

    commit 6

---
+++ Profile/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from Profile.models import Profile
+# Register your models here.
+admin.site.register(Profile)
---
+++ Profile/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Profile(models.Model):
+    firstName = models.CharField(max_length=200, blank=True)
+    lastName = models.CharField(max_length=200, blank=True)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    bio = models.TextField(default="no bio ...",max_length=400)
+    email = models.EmailField(max_length=200,blank=True)
+    country = models.CharField(max_length=200, blank=True)
+    avatar = models.ImageField(default="avatar.png",upload_to='avatar/')
+    friends=models.ManyToManyField(User, blank=True, related_name="friends")
+    slug = models.SlugField(unique=True,blank=True)
+    created = models.DateTimeField(auto_now_add = True)
+    updated = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return f"{self.user.username}-{self.created}"
